00b111eb61366c86d95acd57da27624038dabf63
DwiPutraA <124551419+22091397025DwiPutraAfrilianto@users.noreply.github.com>
2024-11-07 16:20:30 +0700

    Add files via upload

---
+++ fundamental.js
@@ -0,0 +1,38 @@
+// Menggunakan berbagai tipe data
+
+// String
+let name = "Dwi Putra";
+console.log("Name:", name);
+alert("Nama saya adalah: " + name);
+
+// Number
+let age = 21;
+console.log("Age:", age);
+alert("Umur saya adalah: " + age);
+
+// Boolean
+let isStudent = true;
+console.log("Is student:", isStudent);
+alert("Apakah saya seorang Mahasiswa? " + isStudent);
+
+// Array
+let Food = ["Nasi Goreng", "Mie Goreng", "Ayam Bakar"];
+console.log("Food:", Food);
+alert("Saya suka makanan ini: " + Food.join(", "));
+
+// Object
+let person = {
+  firstName: "Dwi",
+  lastName: "Putra Afrilianto",
+  age: 21
+};
+console.log("Person:", person);
+alert("Nama lengkap: " + person.firstName + " " + person.lastName + ", Umur: " + person.age);
+
+// Null
+let emptyValue = null;
+console.log("Null value:", emptyValue);
+
+// Undefined
+let undefinedValue;
+console.log("Undefined value:", undefinedValue);
